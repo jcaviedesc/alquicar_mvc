caec8882c6381aa3c2fcdec182d7d242bae3768f
--replace--all <jcamilocc97@gmail.com>
2017-05-15 22:17:40 -0500

    creating create user

---
+++ Alquicar_mvc/Controllers/CreateUserController.cs
@@ -0,0 +1,100 @@
+﻿using Alquicar_mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alquicar_mvc.Controllers
+{
+    public class CreateUserController : Controller
+    {
+        CreateUserModel user = new CreateUserModel();
+        // GET: CreateUser
+        public ActionResult Index()
+        {
+            ViewBag.permissions = user.QueryPermissions();
+            ViewBag.roles = user.QueryPermissions();
+            return View();
+        }
+
+        // GET: CreateUser/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CreateUser/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CreateUser/Create
+        [HttpPost]
+        public ActionResult Create(CreateUserModel collection)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    user.RegisterUser(collection);
+                    return RedirectToAction("Index");
+                }
+                else {
+                    return View();
+                }
+
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CreateUser/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CreateUser/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CreateUser/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CreateUser/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Alquicar_mvc/Models/CreateUserModel.cs
@@ -0,0 +1,55 @@
+﻿using conexion;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Alquicar_mvc.Models
+{
+    public class CreateUserModel
+    {
+        Connection conbd = new Connection();
+
+        public string Rol { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string Permissions { get; set; }
+
+        public System.Data.DataTable QueryPermissions()
+        {
+            return conbd.Query("PR_QUERYPERMISSIONS", null);
+        }
+
+        public System.Data.DataTable QueryRoles() {
+            return conbd.Query("PR_QUERYROLES",null);
+        }
+
+        public bool RegisterUser(CreateUserModel user) {
+            Parameter[] para = new Parameter[4];
+            para[0] = new Parameter("_Email",user.Email);
+            para[1] = new Parameter("_Password",user.Password);
+            para[2] = new Parameter("_Permissions", user.Permissions);
+            para[3] = new Parameter("_Rol",user.Rol);
+ 
+            Transaction[] trans = new Transaction[1];
+            trans[0] = new Transaction("PR_CREATEUSER", para);
+            return conbd.Transaction(trans);
+        }
+    }
+}
