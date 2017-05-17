4e4bd7c169b0faaa775cb75b6bbadb6e6403c1c9
juan camilo <jcamilocc97@gmail.com>
2017-05-16 21:13:12 -0500

    add alquilerController view and model

---
+++ Alquicar_mvc/Controllers/AlquilerController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Alquicar_mvc.Controllers
+{
+    public class AlquilerController : Controller
+    {
+        // GET: Alquiler
+        public ActionResult Index()
+        {
+            return View();
+
+        }
+
+        // GET: Alquiler/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Alquiler/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Alquiler/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alquiler/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Alquiler/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Alquiler/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Alquiler/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Alquicar_mvc/Models/AlquilerCarModel.cs
@@ -0,0 +1,30 @@
+﻿using conexion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Alquicar_mvc.Models
+{
+    public class AlquilerCarModel
+    {
+        public string start_date { get; set; }
+        public string final_date { get; set; }
+        public string car_id { get; set; }
+        public string cliente_id { get; set; }
+
+        Connection ConnAlquiler = new Connection();
+        public bool RegisterAlquiler(AlquilerCarModel alquiler)
+        {
+            Parameter[] data_alquiler = new Parameter[4];
+            data_alquiler[0] = new Parameter("_start_date", alquiler.start_date);
+            data_alquiler[1] = new Parameter("_final_date", alquiler.start_date);
+            data_alquiler[2] = new Parameter("_car_id", alquiler.start_date);
+            data_alquiler[3] = new Parameter("_cliente_id", alquiler.start_date);
+
+            Transaction[] trans = new Transaction[1];
+            trans[0] = new Transaction("PR_INSRALQUILER", data_alquiler);
+            return ConnAlquiler.Transaction(trans);
+        }
+    }
+}
