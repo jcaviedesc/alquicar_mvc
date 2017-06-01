e46a67f77d201af69facccbfe7f1a1d08147d731
--replace--all <jcamilocc97@gmail.com>
2017-05-31 19:59:27 -0500

    modifying controller, model and view of registercar

--- Alquicar_mvc/Models/RegisterCarModels.cs
+++ Alquicar_mvc/Models/RegisterCarModels.cs
@@ -28,23 +28,33 @@ namespace Alquicar_mvc.Models
         Connection registracar = new Connection();
 
         public string proveedor_car { get; set; }
+
         public string tipo_vehiculo { set; get; }
+
         public string tipo_direccion { set; get; }
+
         public string tipo_trasmicion { set; get; }
         
         public string marca { get; set; }
         //string gama { set; get; }
         public string color { set; get; }
+
         public string cilindraje { set; get; }
+
         public string modelo { set; get; }
+
         public string descripcion { set; get; }
         //string aire_acondicionado { set; get; }
         //string pasajeros { set; get; }
         //string maletero { set; get; }
         //string tipo_combustible { set; get; }
         public string placa { set; get; }
+
         public string kilomentros { set; get; }
 
+        public string precio_pago { set; get; } 
+
+        public string registrado_por { set; get; }
         //public string rutaImg { set; get; }
         //method return all types of car
         public SelectList get_tiposcar() {
@@ -154,7 +164,7 @@ namespace Alquicar_mvc.Models
 
         public bool RegistrarCar(RegisterCarModels car, string rutaimg)
         {
-            Parameter[] para_car = new Parameter[12];
+            Parameter[] para_car = new Parameter[14];
 
             para_car[0] = new Parameter("_proveedorid", car.proveedor_car);
             para_car[1] = new Parameter("_tipo_vehiculo", car.tipo_vehiculo);
@@ -173,6 +183,8 @@ namespace Alquicar_mvc.Models
             para_car[9] = new Parameter("_placa", car.placa);
             para_car[10] = new Parameter("_ruta_img", rutaimg);
             para_car[11] = new Parameter("_km", car.kilomentros);
+            para_car[12] = new Parameter("_precio_pago",car.precio_pago);
+            para_car[13] = new Parameter("_registradopor", car.registrado_por);
             Transaction[] trans = new Transaction[1];
             trans[0] = new Transaction("PR_REGISTER_CAR", para_car);
             return registracar.Transaction(trans);
